a73d72ce83852cf1ebe6482cf44448a541ad5eed
Bartosz Janiszewski <b.janiszewski@interia.pl>
2022-09-14 13:05:00 +0200

    removed comments

--- github.js
+++ github.js
@@ -3,27 +3,25 @@ let mySecret = config.MY_SECRET;
 
 class Github {
   constructor() {
-    // this.client_id = "9dac8de0d678bcd5a80d";
-    // this.client_secret = "1c93b0b2c274d08a69219840b5fed453ccd7f64c";
     this.repos_count = 5;
-    this.repos_sort = 'created: asc';
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
-    // const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=' + 'myKey' + '&client_secret= 'mySecret'`
+    );
 
-    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=' + 'myKey' + '&client_secret= 'mySecret'`);
-
-    // const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
-
-    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=' + 'myKey' + '&client_secret=' + 'mySecret'`);
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=' + 'myKey' + '&client_secret=' + 'mySecret'`
+    );
 
     const profile = await profileResponse.json();
     const repos = await repoResponse.json();
 
     return {
       profile,
-      repos
-    }
+      repos,
+    };
   }
 }
