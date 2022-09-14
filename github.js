aedee717548981ad863fe2505fadc4d71943b037
Bartosz Janiszewski <b.janiszewski@interia.pl>
2022-09-14 13:03:30 +0200

    security changes

--- github.js
+++ github.js
@@ -1,15 +1,22 @@
+let myKey = config.MY_KEY;
+let mySecret = config.MY_SECRET;
+
 class Github {
   constructor() {
-    this.client_id = "9dac8de0d678bcd5a80d";
-    this.client_secret = "1c93b0b2c274d08a69219840b5fed453ccd7f64c";
+    // this.client_id = "9dac8de0d678bcd5a80d";
+    // this.client_secret = "1c93b0b2c274d08a69219840b5fed453ccd7f64c";
     this.repos_count = 5;
     this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
-    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    // const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=' + 'myKey' + '&client_secret= 'mySecret'`);
+
+    // const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=' + 'myKey' + '&client_secret=' + 'mySecret'`);
 
     const profile = await profileResponse.json();
     const repos = await repoResponse.json();
